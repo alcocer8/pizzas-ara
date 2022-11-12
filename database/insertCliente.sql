5f7e8d3b95b058a1164b7dc581a4ccfd0b5a2ef5
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-11-11 18:33:53 -0600

    script para insertar en tabla cliente

---
+++ database/insertCliente.sql
@@ -0,0 +1,6 @@
+INSERT INTO Cliente (name, lastName, phone, calle, colonia, 
+nInterior, nExterior, cp, referencias, idMunicipio, idCiudad,
+idEstado)
+VALUES
+('Roberto', 'Lagunes', '2299533808', 'Su calle', 'X colonia', 4, 0,
+'76849', 'Referencia', 3, 2, 1);
