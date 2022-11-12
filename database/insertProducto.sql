00303391fb8f8e671615b3192cb96ce74db195af
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-11-11 18:34:32 -0600

    script para insertar en tabla producto

---
+++ database/insertProducto.sql
@@ -0,0 +1,4 @@
+INSERT INTO Producto (name, descripcion, precio, quantity)
+VALUES
+('Hot n Ready', 'Pizza clasica de pepperoni', 89.00, 10),
+('Hawaian Chicken', 'Jamon, tocino, pollo y salsa manga habanero', 159.00, 5);
