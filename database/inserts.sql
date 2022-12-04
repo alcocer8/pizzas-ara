58b5c8682067e1a09c9193b21203c92aa2997dd9
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-12-03 19:13:43 -0600

    agrega contraseña para el cliente

--- database/inserts.sql
+++ database/inserts.sql
@@ -287,11 +287,11 @@ INSERT INTO Estado (NombreEstado)
 VALUES
 ('Veracruz');
 
-INSERT INTO Cliente (name, lastName, phone, email, calle, colonia, 
+INSERT INTO Cliente (name, lastName, phone, email, pass, calle, colonia, 
 nInterior, nExterior, cp, referencias, idMunicipio, idCiudad,
 idEstado)
 VALUES
-('Roberto', 'Lagunes', '2299533808', 'robert@correo.com', 'Su calle', 'X colonia', 4, 0,
+('Roberto', 'Lagunes', '2299533808', 'robert@correo.com', 'robertFront','Su calle', 'X colonia', 4, 0,
 '76849', 'Referencia', 3, 2, 1);
 
 INSERT INTO Cargo (name)
