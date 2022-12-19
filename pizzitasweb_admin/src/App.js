24fcba60bc73019eb640c2c940efe242530e97c6
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2022-12-18 23:50:25 -0600

    actualiza imports

--- pizzitasweb_admin/src/App.js
+++ pizzitasweb_admin/src/App.js
@@ -3,14 +3,14 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 //Importamos los components creados
 import NavBarExample from './components/layouts/navbar';
 import Home from './components/Home';
-import EmployeesManagement from './components/EmployeesManagement';
-import ProductsManagement from './components/ProductsManagement';
+import EmployeesManagement from './components/employees/EmployeesManagement';
+import ProductsManagement from './components/products/ProductsManagement';
 import OrdersManagement from './components/OrdersManagement';
-import Employees from './components/Employees';
-import AddEmployees from './components/AddEmployees';
-import EditEmployees from './components/EditEmployees';
-import EditProduct from './components/EditProduct';
-import AddProduct from './components/AddProduct';
+import Employees from './components/employees/Employees';
+import AddEmployees from './components/employees/AddEmployees';
+import EditEmployees from './components/employees/EditEmployees';
+import EditProduct from './components/products/EditProduct';
+import AddProduct from './components/products/AddProduct';
 
 
 function App() {
