e8c673239e6652e1596f87d93cc625104bb5c953
Eduardo Ruiz Rios <eduardo.ruiz.rios09@gmail.com>
2023-01-02 18:39:05 -0600

    elimina la ConnectionString del contexto

--- webapi/StoreManagement/Models/entities/PizzitasContext.cs
+++ webapi/StoreManagement/Models/entities/PizzitasContext.cs
@@ -39,10 +39,6 @@ public partial class PizzitasContext : DbContext
 
     public virtual DbSet<Sucursalempleado> Sucursalempleados { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("server=localhost;port=5432;username=lorenzo;password=1234;database=pizzitas");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Cargo>(entity =>
